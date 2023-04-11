b38123703672366109b204b556249db160d8caa3
Roberto <roberto.coviello@bearingpoint.com>
2023-04-11 15:58:20 +0700

    Update Starter.tsx

--- src/worlds/Starter.tsx
+++ src/worlds/Starter.tsx
@@ -1,15 +1,19 @@
-import { Spinning, Floating, StandardReality, Model } from "spacesvr";
+import { Spinning, Floating, StandardReality, Model, Interactable } from "spacesvr";
 import TransparentFloor from "ideas/TransparentFloor";
 import CloudySky from "ideas/CloudySky";
 import {Sky, Cloud} from "@react-three/drei";
 
 export default function Starter() {
   return (
-    <StandardReality>
+    <StandardReality playerProps={{
+      pos: [0,1,1]
+    }}>
       <ambientLight intensity={4}/>
       <Model scale={0.007} position-y = {0.01} rotation-x={-Math.PI/2} src = "./vrShowcase.glb"></Model>
       <Spinning ySpeed={1.5}>
-      <Model scale={0.09} position-y={0.9} position-z={-0.03} src="./react_logo.glb"></Model>
+        <Interactable onClick={() => window.open("https://docs.pmnd.rs/react-three-fiber/getting-started/introduction!")}>
+          <Model scale={0.09} position-y={0.9} position-z={-0.03} src="./react_logo.glb"></Model>
+        </Interactable>
       </Spinning>
       <TransparentFloor opacity={0.7} />
       <Sky sunPosition={[0,1,0]}></Sky>
