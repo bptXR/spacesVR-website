8de38ce0fcf5ae1ad7ed34dd212799687b0f4d32
Roberto <roberto.coviello@bearingpoint.com>
2023-04-11 16:25:09 +0700

    Update Starter.tsx

--- src/worlds/Starter.tsx
+++ src/worlds/Starter.tsx
@@ -7,10 +7,10 @@ export default function Starter() {
     <StandardReality playerProps={{
       pos: [0,1,1]
     }}>
-      <ambientLight/>
+      <ambientLight intensity={4}/>
       <Model scale={0.007} position-y = {0.01} rotation-x={-Math.PI/2} src = "./vrShowcase.glb"></Model>
       <Spinning ySpeed={1.5}>
-        <Interactable onClick={() => window.open("https://docs.pmnd.rs/react-three-fiber/getting-started/introduction")}>
+        <Interactable onClick={() => window.open("https://docs.pmnd.rs/react-three-fiber/getting-started/introduction!")}>
           <Model scale={0.09} position-y={0.9} position-z={-0.03} src="./react_logo.glb"></Model>
         </Interactable>
       </Spinning>
